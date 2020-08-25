e9d0c00535ce3de2c6b0912a4c5fcc7525a585d3
ekarahodza1 <ekarahodza1@etf.unsa.ba>
2020-08-25 20:37:08 +0200

    napravljen fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
     }
 
