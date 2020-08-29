1aab407bcb6d35b3a8b46af06a1c434ee83e7b05
ekarahodza1 <ekarahodza1@etf.unsa.ba>
2020-08-29 22:17:26 +0200

    pocet glavni kontroler

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -1,4 +1,32 @@
 package sample.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import sample.dto.Product;
+import sample.models.ProductModel;
+
 public class MainController {
+
+    public TableView<Product> tableViewStorage;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colType;
+    public TableColumn colAmount;
+    public TableColumn colPrice;
+    public TableColumn colPriceAll;
+    public TableColumn colStorage;
+    public TableColumn colDate;
+    public TableColumn colDateExp;
+
+    private ProductModel model;
+    private ObservableList<Product> listProduct;
+
+    public MainController() {
+        model = ProductModel.getInstance();
+        listProduct = FXCollections.observableArrayList(model.getProducts());
+    }
+
+
 }
