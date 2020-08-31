f2576582e3b6edcf99a0f2d41341396599221bd3
ekarahodza1 <ekarahodza1@etf.unsa.ba>
2020-09-01 00:30:04 +0200

    kontroler za warehouse

--- src/sample/controllers/WarehouseController.java
+++ src/sample/controllers/WarehouseController.java
@@ -1,9 +1,50 @@
 package sample.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import sample.dto.Warehouse;
 
 public class WarehouseController {
+    public TextField fieldName;
+    public Warehouse warehouse;
+
+    public WarehouseController() {}
+
+    @FXML
+    public void initialize() {
+
+    }
+
+
     public Warehouse getWarehouse() {
-        return null;
+        return warehouse;
+    }
+
+    public void clickCancel(ActionEvent actionEvent) {
+        warehouse = null;
+        Stage stage = (Stage) fieldName.getScene().getWindow();
+        stage.close();
+    }
+
+    public void clickOk(ActionEvent actionEvent) {
+        boolean Ok = true;
+
+        if (fieldName.getText().trim().isEmpty()) {
+            fieldName.getStyleClass().removeAll("poljeIspravno");
+            fieldName.getStyleClass().add("poljeNijeIspravno");
+            Ok = false;
+        } else {
+            fieldName.getStyleClass().removeAll("poljeNijeIspravno");
+            fieldName.getStyleClass().add("poljeIspravno");
+        }
+
+        if (!Ok) return;
+
+        if (warehouse == null) warehouse = new Warehouse();
+        warehouse.setName(fieldName.getText());
+        Stage stage = (Stage) fieldName.getScene().getWindow();
+        stage.close();
     }
 }
